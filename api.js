caff5819100fe6d2d659a62c2fd8b00c89083448
Shinji Takae <shnjtk@gmail.com>
2019-10-21 12:15:53 +0900

    Initial commit

---
+++ api.js
@@ -0,0 +1,22 @@
+const Api = require('claudia-api-builder');
+const api = new Api();
+
+const getPizzas = require('./handlers/get-pizzas');
+
+api.get('/', () => 'Welcome to Aunt Maria Pizzeria API');
+
+api.get('/pizzas', () => {
+    return getPizzas();
+});
+
+api.get(
+    '/pizzas/{id}',
+    request => {
+        return getPizzas(request.pathParams.id);
+    },
+    {
+        error: 404,
+    }
+);
+
+module.exports = api;
