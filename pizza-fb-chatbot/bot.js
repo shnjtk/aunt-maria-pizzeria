c720edc63cf8220bec81255117ada73fd98b6b1e
Shinji Takae <shnjtk@gmail.com>
2019-11-05 12:12:53 +0900

    Finish Chapter 8

---
+++ pizza-fb-chatbot/bot.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const pizzas = require('./data/pizzas.json');
+
+const botBuilder = require('claudia-bot-builder');
+const fbTemplate = botBuilder.fbTemplate;
+
+const api = botBuilder(
+    () => {
+        const message = new fbTemplate.Generic();
+
+        pizzas.forEach(pizza => {
+            message
+                .addBubble(pizza.name)
+                .addImage(pizza.image)
+                .addButton('Details', pizza.id);
+        });
+
+        return ["Hello, here's our pizza menu:", message.get()];
+    },
+    {
+        platform: ['facebook'],
+    }
+);
+
+module.exports = api;
